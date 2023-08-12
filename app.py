28fdb57f006d30406c9cffdf0520f5bdef76837b
NarendraPatil81 <67905859+NarendraPatil81@users.noreply.github.com>
2023-08-12 11:49:57 +0530

    Update app.py

--- app.py
+++ app.py
@@ -41,9 +41,9 @@ hide_streamlit_style = """
             """
 st.markdown(hide_streamlit_style, unsafe_allow_html=True)
 
-#"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"
-os.environ['OPENAI_API_KEY'] = "enter-api-key"
-openai.api_key="enter-api-key"
+
+os.environ['OPENAI_API_KEY'] = "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"
+openai.api_key="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"
 from streamlit_chat import message
 global docs
 from langchain.text_splitter import CharacterTextSplitter
